d01b159b264fe8e856178c5585b6d226dd281fbd
JOÃO VITOR VIEIRA WAICAMPE <joao_waicampe@estudante.sesisenai.org.br>
2026-08-20 10:13:24 -0300

    adicionando destroy (delete)

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,7 @@ routes.get("/", (req, res) => {
 routes.get("/produtos", produtoController.index);
 routes.post("/produtos", produtoController.store);
 routes.put("/produtos/:id", produtoController.update);
+routes.delete("/produtos/:id", produtoController.destroy);
 
 module.exports = routes;
 
